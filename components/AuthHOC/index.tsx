bf2cc504519b346eb231f942d9688bdec1c6f057
DevNickJr <nicholasduadei14@gmail.com>
2023-05-15 19:44:23 +0100

    setup auth

---
+++ components/AuthHOC/index.tsx
@@ -0,0 +1,35 @@
+import React, { ReactNode } from "react";
+import { useRouter } from "next/router";
+// import useUser from "@/hooks/useUser";
+import { useSession, signIn, signOut } from "next-auth/react"
+
+
+export default function AuthHOC(
+  ProtectedComponent: React.FC<any>,
+) {
+  //
+  return () => {
+    const session = useSession()
+    // console.log("session", session)
+    const router = useRouter();
+
+    // const user = useUser({
+    //   tryOnce: true,
+    // });
+
+    if (session.status === "loading") {
+      return (
+        <div>
+          <p>Loading...</p>
+        </div>
+      );
+    }
+
+    if (!session.data) {
+      signIn()
+      return null;
+    }
+
+    return <ProtectedComponent user={session} />;
+  };
+}
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,55 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  secret: process.env.AUTH_SECRET || "my-secret",
+  pages: {
+    signIn: "/login",
+    // signOut: '/auth/signout',
+    // error: '/auth/error', // Error code passed in query string as ?error=
+    // verifyRequest: '/auth/verify-request', // (used for check email message)
+    // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // console.log({ credentials, req })
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
+              method: "POST",
+              headers: {
+                  "Content-Type": "application/json"
+              },
+              body: JSON.stringify(credentials)
+          })
+        const { user } = await res.json();
+
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          console.log({ user })
+          return user;
+        }
+        // Return null if user data could not be retrieved
+        return null;
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
