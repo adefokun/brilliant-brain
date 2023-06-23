a385fdfc5d456880c8307bc1cde80136f9f80f7f
DevNickJr <nicholasduadei14@gmail.com>
2023-06-23 12:37:03 +0100

    .

--- layouts/AdminLayout/index.tsx
+++ layouts/AdminLayout/index.tsx
@@ -6,7 +6,7 @@ const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   
 
   return (
-      <div className='flex bg-footer-bg overflow-hidden lg:h-screen w-full'>
+      <div className='flex bg-footer-bg overflow-hidden h-screen w-full'>
         <SideNav />
         <div className="flex-1 overflow-y-auto overflow-hidden bg-black/5 relative pt-12">
           <Head />
