9cb1285e5952c0151387034c6ba26070fe288e6b
DevNickJr <nicholasduadei14@gmail.com>
2023-06-28 21:03:41 +0100

    .

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -31,7 +31,7 @@ export const authOptions = {
         // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
         // You can also use the `req` object to obtain additional parameters
         // (i.e., the request IP address)
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
+        const res = await fetch(`/api/login`, {
               method: "POST",
               headers: {
                   "Content-Type": "application/json"
